612cef66ae8453563975f1584a35e8ed4aea3438
Eduardo <el_alienx@hotmail.com>
2022-03-28 11:41:34 +0200

    added map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,18 +1,22 @@
 // NPM Packages
-import { Map as LeafletMap, Marker, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 export default function Map({ latitude, longitude }) {
   // Properties
-  const coordinates = [latitude, longitude];
-  const zoomLevel = 10;
+  const position = [latitude, longitude];
+  const zoom = 10;
   const attribution =
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-  const mapURL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+  const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
   return (
-    <LeafletMap className="map" center={coordinates} zoom={zoomLevel}>
-      <TileLayer url={mapURL} attribution={attribution} />
-      <Marker position={coordinates} />
-    </LeafletMap>
+    <MapContainer center={position} zoom={zoom}>
+      <TileLayer attribution={attribution} url={url} />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
