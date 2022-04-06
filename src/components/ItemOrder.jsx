1182f8064808f8933774b16e588d0b62ecc8b244
Eduardo <el_alienx@hotmail.com>
2022-04-06 11:23:10 +0200

    added styling to the item order component

--- src/components/ItemOrder.jsx
+++ src/components/ItemOrder.jsx
@@ -1,10 +1,11 @@
 // NPM packages
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHandPeace } from "@fortawesome/free-regular-svg-icons";
 
 export default function ItemOrder({ item }) {
   return (
     <Link className="item-order" to={`order/${item.id}`}>
-      {/* Packet name */}
       <div className="details sub-container">
         <h2 className="title">{item.sender}</h2>
         <span className="id">Parcel #{item.parcel_id}</span>
@@ -12,16 +13,14 @@ export default function ItemOrder({ item }) {
 
       <div className="spacer">{/* empty on purpose */}</div>
 
-      {/* Status */}
       <div className="status sub-container">
         <div className="icon">📦</div>
         <span>{item.status}</span>
       </div>
 
-      {/* Go arrow */}
-      <div className="sub-container">
-        <span>➡️</span>
-      </div>
+      <span className="sub-container">
+        <FontAwesomeIcon icon={faHandPeace} />
+      </span>
     </Link>
   );
 }
