53d89675eec0375bb76ce9bd6b54538cbbf10a74
Eduardo <el_alienx@hotmail.com>
2022-04-08 10:07:00 +0200

    creating order details

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,4 +1,4 @@
-export default function ItemDetail({ label, text, icon }) {
+export default function ItemDetail({ label = "", text, icon = "" }) {
   // Safeguard
   if (text === null) return null;
   if (text === false) return null;
@@ -7,8 +7,11 @@ export default function ItemDetail({ label, text, icon }) {
   return (
     <div className="item-detail">
       {/* Font awesome goes here */}
-      <span className="label">{label}:</span>
-      <p>{text}</p>
+      <div className="icon">ℹ️</div>
+      <div className="content">
+        <span className="label">{label}</span>
+        <p>{text}</p>
+      </div>
     </div>
   );
 }
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -13,12 +13,11 @@ export default function Details({ orders }) {
 
   // Properties
   const order = orders.filter((item) => item.id === Number(id))[0];
+  if (order === undefined) return <p>The order {id} is not longer available</p>;
+
   const eta = <Moment date={order.eta} format="MMM Do YYYY" />;
   const lastUpdate = <Moment date={order.last_updated} format="MMM Do YYYY" />;
   const sign = order.verification_required;
-
-  // Safeguard
-  if (order === undefined) return <p>The order {id} is not longer available</p>;
 
   return (
     <div id="details" className="container">
@@ -30,11 +29,13 @@ export default function Details({ orders }) {
           latitude={order.location_coordinate_latitude}
           longiture={order.location_coordinate_longitude}
         />
-        <ItemDetail label="Location" text={order.location_name} icon="pin" />
-        <ItemDetail label="Estimated delivery time" text={eta} icon="truck" />
-        <ItemDetail label="Last update" text={lastUpdate} icon="stopwatch" />
-        <ItemDetail label="Notes" text={order.notes} icon="sticky" />
-        <ItemDetail label="Signature required" text={sign} icon="info-circle" />
+        <div className="information">
+          <ItemDetail label="Location" text={order.location_name} icon="pin" />
+          <ItemDetail label="Estimated delivery time" text={eta} icon="truck" />
+          <ItemDetail label="Last update" text={lastUpdate} icon="stopwatch" />
+          <ItemDetail label="Notes" text={order.notes} icon="sticky" />
+          <ItemDetail label="Signature needed" text={sign} icon="info-circle" />
+        </div>
       </section>
       <Link to="/">Go back</Link>
     </div>
